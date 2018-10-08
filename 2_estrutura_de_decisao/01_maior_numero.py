9aa9d8d8e0b0477ebc22dcad7c89f03ce0ecbdde
CECIL MALONE SOUZA CARDOZO <cecil.cardozo@fnde.gov.br>
2018-10-08 17:19:33 -0300

    Inclusão dos exercicios resolvidos para a estrutura de decisão.

---
+++ 2_estrutura_de_decisao/01_maior_numero.py
@@ -0,0 +1,13 @@
+"""
+1. Faça um Programa que peça dois números e imprima o maior deles.
+"""
+
+n1 = int(input())
+n2 = int(input())
+
+if n1 > n2:
+    print(n1)
+elif n2 > n1:
+    print(n2)
+else:
+    print(n1)
---
+++ 2_estrutura_de_decisao/02_positivo_negativo.py
@@ -0,0 +1,10 @@
+"""
+2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+"""
+
+n = int(input())
+
+if n >= 0:
+    print('Positivo')
+else:
+    print('Negativo')
