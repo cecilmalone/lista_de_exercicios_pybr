627ef603e5f2eb29a3c819fa343ba503728f6fb4
CECIL MALONE SOUZA CARDOZO <cecil.cardozo@fnde.gov.br>
2018-10-10 18:28:53 -0300

    Atualizando os exercicios resolvidos para a estrutura de decisão.

--- 2_estrutura_de_decisao/18_data_valida.py
+++ 2_estrutura_de_decisao/18_data_valida.py
@@ -3,13 +3,18 @@
 mesma é uma data válida.
 """
 
-data = input("Informe uma data no formato dd/mm/aaaa": )
+data = input("Informe uma data no formato dd/mm/aaaa: ")
 
-if (int(data[0:1]) < 1) and (int(data[0:1]) > 31):
+
+if (int(data[3:4]) == 2) and (int(data[0:1]) > 29) and ((int(data[6:9]) % 4 == 0) and (int(data[6:9]) % 100 != 0) and (int(data[6:9]) % 400 != 0)):
+    print("Data inválida")
+elif (int(data[3:4]) == 2) and (int(data[0:1]) > 28):
+    print("Data inválida")
+elif (int(data[0:1]) < 1) and (int(data[0:1]) > 31):
     print("Data inválida")
 elif (int(data[3:4]) < 1) and (int(data[:1]) > 12):
     print("Data inválida")
-elif int(data[6:9]) < 1900):
+elif int(data[6:9]) < 1900:
     print("Data inválida")
 else:
     print("Data válida")
